1188e892b49f93cc4c2004f6658e5038c895eb30
ycwu0609 <winie850609@gmail.com>
2017-04-15 19:48:29 +0800

    Add files via upload

---
+++ LAB1/Queue.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int Queue[5];
+	int front,rear,s;
+	front=rear=-1;
+	
+	do
+	{
+		int n;
+		printf("1.push 2.pop 3.print 4.exit:");
+		scanf("%d",&s);
+		if(s==1)
+		{
+			
+			scanf("%d",&n);
+			
+	        if(rear>=4)
+	        {
+	         printf("full\n");
+	        }
+	        
+            else
+            {
+    	      rear++;
+			  Queue[rear]=n;
+    	      
+			  
+    	      
+            }
+		}
+		else if(s==2)
+		{
+			front++;
+			if(rear<0)
+			{
+				printf("empty\n");
+			}
+			
+			else
+			{
+				
+				rear--;
+				printf("%d\n",Queue[0]);
+				for(int i=0;i<=rear;i++)
+				{	
+				   
+					
+						Queue[i]=Queue[i+1];
+						
+				}
+				Queue[rear+1]=NULL;
+			}
+			
+		}
+		else if(s==3)
+		{
+			for(int j=0;j<=4;j++)
+			{
+				printf("%d ",Queue[j]);
+			}
+		   printf("\n");
+		}
+		else
+		{
+			break;
+		}
+	}
+	while(s>=0);
+	
+	
+}
+
+
+
+
