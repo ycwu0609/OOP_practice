5e1f812102299d523d063236467cbe9197900353
Wu Yu-Chen <winie850609@gmail.com>
2017-04-23 01:46:45 +0800

    Update Queue.cpp
    delete null lines

--- LAB1/Queue.cpp
+++ LAB1/Queue.cpp
@@ -1,78 +1,46 @@
 #include <stdio.h>
 
-
 int main(void)
 {
 	int Queue[5];
 	int front,rear,s;
-	front=rear=-1;
-	
+	front=rear=-1;	
 	do
 	{
 		int n;
 		printf("1.push 2.pop 3.print 4.exit:");
 		scanf("%d",&s);
-		if(s==1)
-		{
-			
+		if(s==1){
 			scanf("%d",&n);
-			
-	        if(rear>=4)
-	        {
-	         printf("full\n");
-	        }
-	        
-            else
-            {
-    	      rear++;
-			  Queue[rear]=n;
-    	      
-			  
-    	      
-            }
+	        	if(rear>=4)
+	        		printf("full\n");
+            		else{
+    	      			rear++;
+			  	Queue[rear]=n;
+            		}
 		}
-		else if(s==2)
-		{
+		else if(s==2){
 			front++;
 			if(rear<0)
-			{
 				printf("empty\n");
-			}
-			
-			else
-			{
-				
+			else{
 				rear--;
 				printf("%d\n",Queue[0]);
-				for(int i=0;i<=rear;i++)
-				{	
-				   
-					
-						Queue[i]=Queue[i+1];
-						
-				}
+				for(int i=0;i<=rear;i++)	
+				   	Queue[i]=Queue[i+1];
 				Queue[rear+1]=NULL;
 			}
-			
 		}
-		else if(s==3)
-		{
+		else if(s==3){
 			for(int j=0;j<=4;j++)
-			{
 				printf("%d ",Queue[j]);
-			}
-		   printf("\n");
+			
+		   	printf("\n");
 		}
 		else
-		{
 			break;
-		}
+		
 	}
 	while(s>=0);
 	
-	
 }
-
-
-
-
